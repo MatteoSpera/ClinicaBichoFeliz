cad14e5dfc11e1073a0cfce5033cdaf430771a44
MatteoSpera <matteo.spera0301@gmail.com>
2023-06-28 18:01:40 -0300

    Menu Criado

---
+++ menu.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"><!--link para lib de ícones-->
+
+</head>
+
+<body>
+    <nav>
+    <div class="nav-wrapper light-green accent-2">
+      <a href="#" class="brand-logo right"><img src="/ClinicaBichoFeliz/img/LogoBichoFeliz.png" alt="Site Logo" width="65" height="65"></a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down ">
+        <li><a href="/ClinicaBichoFeliz/VIEW/Animal/lstAnimal.php" class="black-text">Animal</a></li>
+      </ul>
+    </div>
+  </nav>
+</body>
+
+</html>
