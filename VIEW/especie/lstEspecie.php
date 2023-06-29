29bce985aa752a91ea8dbca68cf50b6d1caff9d7
MatteoSpera <matteo.spera0301@gmail.com>
2023-06-29 00:27:33 -0300

    Listagem de Espécies Criada com Refresh em Tempo real de Espécies

---
+++ VIEW/especie/lstEspecie.php
@@ -0,0 +1,67 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/ClinicaBichoFeliz/BLL/bllEspecie.php'; 
+use BLL\bllEspecie;
+$bll = new \BLL\bllEspecie();
+$lstEspecie = $bll->Select();
+
+$bll->refreshAll();
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <!--link para lib de ícones-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+    <title>Listar Espécies</title>
+</head>
+
+<body>
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/ClinicaBichoFeliz/VIEW/menu.php'; ?>
+
+    <div class="container">
+        <h1>Relatório de Espécies de Pacientes</h1><br>
+        <h5>Atualizar <a class="btn-floating btn-small waves-effect waves-light light-green accent-3" href=""><i class="material-icons">refresh</i></a></h5>
+
+        <table class="striped light-green lighten-3">
+
+
+            <tr>
+                <th>ID</th>
+                <th>Espécie</th>
+                <th>Quantidade</th>
+            </tr>
+            <?php
+
+            // para cada pet na lista, cria uma nova linha na tabela html
+            foreach ($lstEspecie as $especie) { // foreach abre dentro de uma tag e fecha na outra
+
+            ?>
+
+                <tr>
+                    <td><?php echo $especie->getId(); ?></td>
+                    <td><?php echo $especie->getDescricao(); ?></td>
+                    <td><?php echo $especie->getQuantidade(); ?></td>
+                </tr>
+            <?php
+            } // <--- Chave de fechamento do Foreach
+            ?>
+
+        </table>
+    </div>
+    <br><br>
+
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/ClinicaBichoFeliz/VIEW/footer.php'; ?>
+
+</body>
+
+</html>
