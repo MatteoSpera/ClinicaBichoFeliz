a4e3e4c36b9f0f505d69faebc4dc7d3f4583a355
MatteoSpera <matteo.spera0301@gmail.com>
2023-06-29 10:55:28 -0300

    Criado Listar Condições

---
+++ VIEW/condicao/lstCondicao.php
@@ -0,0 +1,69 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/ClinicaBichoFeliz/BLL/bllCondicao.php'; 
+use BLL\bllCondicao;
+$bll = new \BLL\bllCondicao();
+
+$bll->refreshAll();
+
+$lstCondicao = $bll->Select();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <!--link para lib de ícones-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+    <title>Condições</title>
+</head>
+
+<body>
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/ClinicaBichoFeliz/VIEW/menu.php'; ?>
+
+    <div class="container">
+        <h1>Relatório de Condições de Pacientes</h1><br>
+        <h5>Atualizar <a class="btn-floating btn-small waves-effect waves-light light-green accent-3" href="javascript:location.reload();"><i class="material-icons">refresh</i></a></h5>
+
+        <table class="striped light-green lighten-3">
+
+
+            <tr>
+                <th>ID</th>
+                <th>Condição</th>
+                <th>Quantidade</th>
+            </tr>
+            <?php
+
+            // para cada pet na lista, cria uma nova linha na tabela html
+            foreach ($lstCondicao as $condicao) { // foreach abre dentro de uma tag e fecha na outra
+
+            ?>
+
+                <tr>
+                    <td><?php echo $condicao->getId(); ?></td>
+                    <td><?php echo $condicao->getDescricao(); ?></td>
+                    <td><?php echo $condicao->getQuantidade(); ?></td>
+                </tr>
+            <?php
+            } // <--- Chave de fechamento do Foreach
+            ?>
+
+        </table>
+    </div>
+    <br><br>
+
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/ClinicaBichoFeliz/VIEW/footer.php'; ?>
+
+</body>
+
+</html>
