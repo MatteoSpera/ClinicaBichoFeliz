6633159c8eb290663a0c2d3e569eea1309c8287c
MatteoSpera <matteo.spera0301@gmail.com>
2023-06-29 19:33:59 -0300

    - Nome do Usuário Logado aparece no Menu - Símbolo do Logout - Logo agora fica no centro

--- VIEW/menu.php
+++ VIEW/menu.php
@@ -2,6 +2,10 @@
 /*Impede a Página de ser acessada caso não haja uma sessão válida */ 
 include_once $_SERVER['DOCUMENT_ROOT'].'/ClinicaBichoFeliz/VIEW/login/restrict.php'; ?>
 
+<?php
+  $usuario = $_SESSION['login'];
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -20,12 +24,14 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/ClinicaBichoFeliz/VIEW/login/restrict.p
 <body>
     <nav>
     <div class="nav-wrapper white">
-      <a href="/ClinicaBichoFeliz/" class="brand-logo right"><img src="/ClinicaBichoFeliz/img/LogoBichoFeliz.png" alt="Site Logo" width="60" height="60"></a>
+    <a href="/ClinicaBichoFeliz/" class="brand-logo center"><img src="/ClinicaBichoFeliz/img/LogoBichoFeliz.png" alt="Site Logo" width="60" height="60"></a>
       <ul id="nav-mobile" class="left hide-on-med-and-down ">
         <li><a href="/ClinicaBichoFeliz/VIEW/Animal/lstAnimal.php" class="black-text">Animais</a></li>
         <li><a href="/ClinicaBichoFeliz/VIEW/Especie/lstEspecie.php" class="black-text">Espécies</a></li>
         <li><a href="/ClinicaBichoFeliz/VIEW/Condicao/lstCondicao.php" class="black-text">Condições</a></li>
-        <li><a href="/ClinicaBichoFeliz/?logout=true" class="black-text">Logout</a></li>
+      </ul>
+      <ul id="nav-mobile" class="right hide-on-med-and-down ">
+        <li><a class="waves-effect waves-light btn black white-text" href="/ClinicaBichoFeliz/?logout=true"><i class="material-icons right">logout</i><?php echo $usuario; ?></a></li>
       </ul>
     </div>
   </nav>
